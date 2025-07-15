5c923b523224902818f13a449f54ece64884cc74
Vadim Elshin <elshinvadim@gmail.com>
2025-07-15 19:02:34 +0200

    Watcher 3 lesson/Dependency Injection: Introduction & Resolution Modifiers & Value Providers

---
+++ AngularLessons3/src/app/container/container.component.ts
@@ -0,0 +1,25 @@
+import { AfterContentInit, Component, OnInit, ContentChild, Host } from '@angular/core';
+import { EmployeeComponent } from '../employee/employee.component';
+import { RoomsService } from '../rooms/services/rooms.service';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss'],
+  providers: [RoomsService]
+})
+export class ContainerComponent implements OnInit, AfterContentInit{
+
+
+  @ContentChild(EmployeeComponent) employee! : EmployeeComponent 
+
+  constructor(@Host() private roomsService: RoomsService) { }
+
+  ngAfterContentInit(): void {
+    this.employee.empName = 'Vadim'
+  }
+
+  ngOnInit(): void {
+  }
+
+}
